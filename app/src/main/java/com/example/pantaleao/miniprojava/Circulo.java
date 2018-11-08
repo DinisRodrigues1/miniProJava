ffff2ee07644826ddc036ac6c0935273018d7093
DinisRodrigues1 <joaodtrodrigues@ua.pt>
2018-11-08 16:37:14 +0000

    Fixed circle error

--- app/src/main/java/com/example/pantaleao/miniprojava/Circulo.java
+++ app/src/main/java/com/example/pantaleao/miniprojava/Circulo.java
@@ -5,12 +5,14 @@ public class Circulo {
     float centerX;
     float centerY;
 
-    public Circulo(float radius, float x, float y){
+    public Circulo(float radius, float x, float y) {
         this.centerX = x;
         this.centerY = y;
         this.r = radius;
     }
 
-    public float perimeter(Circulo a) { return (float) (2 * Math.PI * a.r); }
+    public float perimeter(Circulo a) {
+        return (float) (2 * Math.PI * a.r);
+    }
+}
 
-    p
