90d6271d17f28559ecd9c71851ed54d02a33b718
DinisRodrigues1 <joaodtrodrigues@ua.pt>
2018-11-11 16:34:38 +0000

    Added drawPanel

--- app/src/main/java/com/example/pantaleao/miniprojava/MainActivity.java
+++ app/src/main/java/com/example/pantaleao/miniprojava/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.pantaleao.miniprojava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    drawPanel v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        v = findViewById(R.id.painel);
+    }
+    public void f(View vs)
+    {
+        v.setfigure(1);
+        v.invalidate();
     }
 }
---
+++ app/src/main/java/com/example/pantaleao/miniprojava/drawPanel.java
@@ -0,0 +1,52 @@
+package com.example.pantaleao.miniprojava;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class drawPanel extends View {
+    Paint paint = null;
+    int figure;
+    public drawPanel(Context context) {
+        super(context);
+        paint = new Paint();
+        figure=0;
+    }
+
+    public drawPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+        figure =0;
+    }
+
+
+    public drawPanel(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        paint = new Paint();
+        figure =0;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int x = getWidth();
+        int y = getHeight();
+        int radius;
+        radius = 100;
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+        // Use Color.parseColor to define HTML colors
+        paint.setColor(Color.parseColor("#CD5C5C"));
+        if (figure ==1)
+            canvas.drawCircle(x / 2, y / 2, radius, paint);
+    }
+
+    public void setfigure(int a){
+        this.figure=a;
+    }
+}
+
