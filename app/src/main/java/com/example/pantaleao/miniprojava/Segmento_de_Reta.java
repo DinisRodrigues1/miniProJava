62bb16116a61e6070523953950d0e13f881eb99a
DinisRodrigues1 <joaodtrodrigues@ua.pt>
2018-11-17 17:34:02 +0000

    modificação de classes para implementação de objetos no drawPanel

--- app/src/main/java/com/example/pantaleao/miniprojava/Segmento_de_Reta.java
+++ app/src/main/java/com/example/pantaleao/miniprojava/Segmento_de_Reta.java
@@ -1,22 +1,22 @@
 package com.example.pantaleao.miniprojava;
 
-public class Segmento_de_Reta extends Ponto2D{
+public class Segmento_de_Reta {
     int color;
     Ponto2D a;
     Ponto2D b;
+    double dist;
 
 
-    public Segmento_de_Reta(int c) {
+    public Segmento_de_Reta(int c, Ponto2D a, Ponto2D b) {
         color = c;
+        this.a = a;
+        this.b = b;
+        dist =  a.distPontos(b);
     }
 
     public double pontoMedio(Ponto2D a, Ponto2D b) {
         return (a.x + b.x) + (a.y + b.y) / 2;
     }
 
-
-
-   /* public double changeColor(int c) {
-        return
-    }*/
+    
 }
