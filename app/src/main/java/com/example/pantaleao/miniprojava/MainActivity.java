d0eccb79b5137bd416afa0d234fd45e188e49922
DinisRodrigues1 <joaodtrodrigues@ua.pt>
2018-11-21 15:52:58 +0000

    is this it?

--- app/src/main/java/com/example/pantaleao/miniprojava/MainActivity.java
+++ app/src/main/java/com/example/pantaleao/miniprojava/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.pantaleao.miniprojava;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    // Declaration of global vars
     private drawPanel panel;
     private String colorCode;
     private String item;
-    private TextView test;
     private Spinner spinner;
     private String selItem;
     private Circulo circulo;
@@ -28,143 +31,160 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // TODO mypol = new Poligono();
-        circulo = new Circulo();
-        reta = new SegmentoReta();
-        mypol = new Poligono();
+
+        // Lock screen orientation
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Get layout components
         setContentView(R.layout.activity_main);
         panel = findViewById(R.id.painel);
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
 
+        // Init the figures objects
+        circulo = new Circulo();
+        reta = new SegmentoReta();
+        mypol = new Poligono();
+
+        // Add items to the spinner
         List<String> options = new ArrayList<String>();
         options.add("Reta");
         options.add("Circulo");
-        options.add("Poligono");
+        options.add("Triangulo");
+        options.add("Poly4");
+        options.add("Poly5");
 
         ArrayAdapter<String> adapter = new  ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, options);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        addTouchListener();
+        panel.setOnTouchListener(handleCanvasTouchListener);
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         // On selecting a spinner item
-        item = parent.getItemAtPosition(pos).toString();
-
-        test = findViewById(R.id.teste);
-        test.setText(item);
-        if (item.equals("Poligono")) {
-            //Toast.makeText(getApplicationContext(), "poligono", Toast.LENGTH_SHORT).show();
-            selItem = item;
-        }
-        else if (item.equals("Circulo")){
-            //Toast.makeText(getApplicationContext(), "circulo", Toast.LENGTH_SHORT).show();
-            selItem = item;
-        }
-        else{
-            //Toast.makeText(getApplicationContext(), "reta", Toast.LENGTH_SHORT).show();
-            selItem = item;
-        }
+        selItem = parent.getItemAtPosition(pos).toString();
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
 
-
+    // Canvas Touch Listener
     @SuppressLint("ClickableViewAccessibility")
-    public void addTouchListener() {
-        panel.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View view, MotionEvent event) {
-
-                if (selItem.equals("Poligono")){
-                    panel.invalidate();
-                   if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                      if(mypol.getLista().size() < 3){
-                          Toast.makeText(getApplicationContext(), "poligono add", Toast.LENGTH_SHORT).show();
-                          mypol.add(new Ponto2D(event.getX(),event.getY()));
-                      }
-                      if(mypol.getLista().size()==3){
-                          panel.invalidate();
-                          panel.setfigure(3,mypol.getLista().get(0),mypol.getLista().get(1),mypol.getLista().get(2));
-                          panel.invalidate();
-                          mypol.getLista().clear();
-                      }
+    private View.OnTouchListener handleCanvasTouchListener = new View.OnTouchListener(){
+        public boolean onTouch(View view, MotionEvent event) {
+            if (selItem.equals("Triangulo")){
+                panel.invalidate();
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if(mypol.getLista().size() < 3){
+                        mypol.add(new Ponto2D(event.getX(),event.getY()));
+                    }
+                    if(mypol.getLista().size()==3){
+                        panel.invalidate();
+                        panel.setfigure(3,mypol.getLista().get(0),mypol.getLista().get(1),mypol.getLista().get(2));
+                        panel.invalidate();
+                        mypol.getLista().clear();
                     }
                 }
-                else if (selItem.equals("Circulo")) {
-                    panel.invalidate();
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        if(circulo.isZeroPoints()){
-                            circulo.setPointOne(new Ponto2D(event.getX(),event.getY()));
-                        }else{
-                            circulo.setPointTwo(new Ponto2D(event.getX(),event.getY()));
-                            circulo.calc();
-                            panel.invalidate();
-                            panel.setfigure(2,circulo.getCenter() ,circulo.getRaio());
-                            panel.invalidate();
-                        }
-                    }
-                }
-                else {
-                    panel.invalidate();
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        if(reta.isZeroPoints()){
-                            reta.setA(new Ponto2D(event.getX(),event.getY()));
-                        }else{
-                            reta.setB(new Ponto2D(event.getX(),event.getY()));
-                            panel.invalidate();
-                            panel.setfigure(1, reta.getA() ,reta.getB());
-                            panel.invalidate();
-                        }
-                    }
-                }
-                return false;
             }
-        });
-    }
+            else if (selItem.equals("Circulo")) {
+                panel.invalidate();
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if(circulo.isZeroPoints()){
+                        circulo.setPointOne(new Ponto2D(event.getX(),event.getY()));
+                    }else{
+                        circulo.setPointTwo(new Ponto2D(event.getX(),event.getY()));
+                        circulo.calc();
+                        panel.invalidate();
+                        panel.setfigure(2,circulo.getCenter() ,circulo.getRaio());
+                        panel.invalidate();
+                    }
+                }
+            }
+            else if (selItem.equals("Reta")) {
+                panel.invalidate();
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if(reta.isZeroPoints()){
+                        reta.setA(new Ponto2D(event.getX(),event.getY()));
+                    }else{
+                        reta.setB(new Ponto2D(event.getX(),event.getY()));
+                        panel.invalidate();
+                        panel.setfigure(1, reta.getA() ,reta.getB());
+                        panel.invalidate();
+                    }
+                }
+            } else if (selItem.equals("Poly4")){
+                panel.invalidate();
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if(mypol.getLista().size() < 4){
+                        mypol.add(new Ponto2D(event.getX(),event.getY()));
+                    }
+                    if(mypol.getLista().size()==4){
+                        panel.invalidate();
+                        panel.setfigure(4,mypol.getLista().get(0),mypol.getLista().get(1),mypol.getLista().get(2),mypol.getLista().get(3));
+                        panel.invalidate();
+                        mypol.getLista().clear();
+                    }
+                }
+            } else {
+                panel.invalidate();
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if(mypol.getLista().size() < 5){
+                        mypol.add(new Ponto2D(event.getX(),event.getY()));
+                    }
+                    if(mypol.getLista().size()==5){
+                        panel.invalidate();
+                        panel.setfigure(5,mypol.getLista().get(0),mypol.getLista().get(1),mypol.getLista().get(2),mypol.getLista().get(3),mypol.getLista().get(4));
+                        panel.invalidate();
+                        mypol.getLista().clear();
+                    }
+                }
+            }
+            return false;
+        }
+    };
 
 
-  /*  public void colorSel(View view){
+   public void colorSel(View view){
         Context context = getApplicationContext();
             switch (view.getId()) {
 
                 case R.id.btnBlack:
-                    colorCode = (String)findViewById(R.id.btnBlack).getTag();
-                    colorVal = "#000000";
-                    test.setText(colorVal);
+                    colorCode = "#000000";
+                    panel.setcolor(colorCode);
+                    panel.invalidate();
                     Toast.makeText(context, colorCode, Toast.LENGTH_LONG).show();
                     break;
                 case R.id.btnBlue:
-                    colorCode = (String)findViewById(R.id.btnBlue).getTag();
-                    colorVal = "#0000FF";
-                    test.setText(colorVal);
+                    colorCode = "#0000FF";
+                    panel.setcolor(colorCode);
+                    panel.invalidate();
                     Toast.makeText(context, colorCode, Toast.LENGTH_LONG).show();
                     break;
                 case R.id.btnGreen:
-                    colorCode = (String)findViewById(R.id.btnGreen).getTag();
-                    colorVal = "#008000";
-                    test.setText(colorVal);
+                    colorCode = "#008000";
+                    panel.setcolor(colorCode);
+                    panel.invalidate();
                     Toast.makeText(context, colorCode, Toast.LENGTH_LONG).show();
                     break;
                 case R.id.btnRed:
-                    colorCode = (String)findViewById(R.id.btnRed).getTag();
-                    colorVal = "#FF0000";
-                    test.setText(colorVal);
+                    colorCode = "#FF0000";
+                    panel.setcolor(colorCode);
+                    panel.invalidate();
                     Toast.makeText(context, colorCode, Toast.LENGTH_LONG).show();
                     break;
                 case R.id.btnClear:
-                    mypol.lista.clear();
-
-                    colorCode = (String)findViewById(R.id.btnClear).getTag();
+                    mypol.getLista().clear();
+                    panel.invalidate();
+                    panel.clearcanvas(6);
                     Toast.makeText(context, "Cleared Interface", Toast.LENGTH_LONG).show();
                     break;
                 case R.id.btnAbout:
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                     builder.setTitle("About");
-                    builder.setMessage("Para criar um poligono selecione a sua opção e carregue no " +
+                    builder.setMessage("Para criar um poligono escolha a sua opção e carregue no " +
                             "ecrã para criar os diversos pontos. \n" +
                             "Para criar um circulo clique e " +
                             "arraste com a opção de circulo selecionada. \n" +
@@ -174,9 +194,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                     break;
             }
-        test = findViewById(R.id.teste);
 
-        }*/
+        }
 
 
 }
--- app/src/main/java/com/example/pantaleao/miniprojava/drawPanel.java
+++ app/src/main/java/com/example/pantaleao/miniprojava/drawPanel.java
@@ -11,6 +11,7 @@ public class drawPanel extends View {
     Paint paint;
     Paint figurePaint;
     int figure;
+    String colorCode;
 
     // variaveis do circulo
     Ponto2D centro;
@@ -24,17 +25,21 @@ public class drawPanel extends View {
     Ponto2D poligPonto1;
     Ponto2D poligPonto2;
     Ponto2D poligPonto3;
+    Ponto2D poligPonto4;
+    Ponto2D poligPonto5;
 
     public drawPanel(Context context) {
         super(context);
         paint = new Paint();
         figure = 0;
+        colorCode = "#FFFFFF";
     }
 
     public drawPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint();
         figure = 0;
+        colorCode = "#FFFFFF";
     }
 
 
@@ -42,6 +47,7 @@ public class drawPanel extends View {
         super(context, attrs, defStyle);
         paint = new Paint();
         figure = 0;
+        colorCode = "#FFFFFF";
     }
 
     @Override
@@ -51,27 +57,63 @@ public class drawPanel extends View {
         paint.setColor(Color.WHITE);
         canvas.drawPaint(paint);
         figurePaint = new Paint();
-        figurePaint.setColor(Color.BLACK);
+
         figurePaint.setStrokeWidth(5);
 
         if (figure == 1) {
             if(linhaPonto1 != null && linhaPonto2 != null){
+                figurePaint.setColor(Color.parseColor(colorCode));
                 canvas.drawLine((float)linhaPonto1.getX(), (float)linhaPonto1.getY(), (float)linhaPonto2.getX(), (float)linhaPonto2.getY(), figurePaint);
             }
         }
         else if (figure == 2) {
             if(centro != null){
+                figurePaint.setColor(Color.parseColor(colorCode));
                 canvas.drawCircle((float)this.centro.getX(), (float)this.centro.getY(), (float)raio, figurePaint);
             }
         }
         else if (figure == 3) {
             if(poligPonto1 !=null && poligPonto2 !=null && poligPonto3 !=null) {
+
                 float[] points = {
                         (float)poligPonto1.getX(), (float)poligPonto1.getY(), (float)poligPonto2.getX(), (float)poligPonto2.getY(),
                         (float)poligPonto2.getX(), (float)poligPonto2.getY(), (float)poligPonto3.getX(), (float) poligPonto3.getY(),
                         (float)poligPonto3.getX(), (float)poligPonto3.getY(), (float)poligPonto1.getX(), (float) poligPonto1.getY()
-                };canvas.drawLines(points, figurePaint);
+                };
+                figurePaint.setStyle(Paint.Style.FILL);
+                figurePaint.setColor(Color.parseColor(colorCode));
+                canvas.drawLines(points, figurePaint);
             }
+        } else if (figure == 4) {
+            if(poligPonto1 !=null && poligPonto2 !=null && poligPonto3 !=null && poligPonto4 !=null) {
+
+                float[] points = {
+                        (float)poligPonto1.getX(), (float)poligPonto1.getY(), (float)poligPonto2.getX(), (float)poligPonto2.getY(),
+                        (float)poligPonto2.getX(), (float)poligPonto2.getY(), (float)poligPonto3.getX(), (float) poligPonto3.getY(),
+                        (float)poligPonto3.getX(), (float)poligPonto3.getY(), (float)poligPonto4.getX(), (float) poligPonto4.getY(),
+                        (float)poligPonto4.getX(), (float)poligPonto4.getY(), (float)poligPonto1.getX(), (float) poligPonto1.getY(),
+                };
+                figurePaint.setStyle(Paint.Style.FILL);
+                figurePaint.setColor(Color.parseColor(colorCode));
+                canvas.drawLines(points, figurePaint);
+            }
+        } else if (figure == 5) {
+            if(poligPonto1 !=null && poligPonto2 !=null && poligPonto3 !=null && poligPonto4 !=null && poligPonto5 != null) {
+
+                float[] points = {
+                        (float)poligPonto1.getX(), (float)poligPonto1.getY(), (float)poligPonto2.getX(), (float)poligPonto2.getY(),
+                        (float)poligPonto2.getX(), (float)poligPonto2.getY(), (float)poligPonto3.getX(), (float) poligPonto3.getY(),
+                        (float)poligPonto3.getX(), (float)poligPonto3.getY(), (float)poligPonto4.getX(), (float) poligPonto4.getY(),
+                        (float)poligPonto4.getX(), (float)poligPonto4.getY(), (float)poligPonto5.getX(), (float) poligPonto5.getY(),
+                        (float)poligPonto5.getX(), (float)poligPonto5.getY(), (float)poligPonto1.getX(), (float) poligPonto1.getY(),
+                };
+                figurePaint.setStyle(Paint.Style.FILL);
+                figurePaint.setColor(Color.parseColor(colorCode));
+                canvas.drawLines(points, figurePaint);
+            }
+        }
+        else if (figure == 6) {
+            paint.setColor(Color.WHITE);
         }
     }
 
@@ -93,6 +135,31 @@ public class drawPanel extends View {
         this.poligPonto2 = ponto2;
         this.poligPonto3 = ponto3;
     }
+
+    public void setfigure(int a, Ponto2D ponto1, Ponto2D ponto2, Ponto2D ponto3, Ponto2D ponto4) {
+        this.figure = a;
+        this.poligPonto1 = ponto1;
+        this.poligPonto2 = ponto2;
+        this.poligPonto3 = ponto3;
+        this.poligPonto4 = ponto4;
+    }
+
+    public void setfigure(int a, Ponto2D ponto1, Ponto2D ponto2, Ponto2D ponto3, Ponto2D ponto4, Ponto2D ponto5) {
+        this.figure = a;
+        this.poligPonto1 = ponto1;
+        this.poligPonto2 = ponto2;
+        this.poligPonto3 = ponto3;
+        this.poligPonto4 = ponto4;
+        this.poligPonto5 = ponto5;
+    }
+
+    public void setcolor(String a) {
+        this.colorCode = a;
+    }
+    public void clearcanvas(int a) {
+        this.figure = a;
+    }
+
 }
 
 
